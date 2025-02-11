b57689bfa2e98142836c7a72a55f9be9f4b5bfd4
heba-webdev <jtvonline@yahoo.com>
2025-02-11 22:19:03 +0300

    fix: 404 error message

--- api/routes/books.py
+++ api/routes/books.py
@@ -67,7 +67,7 @@ async def get_book_by_id(book_id: int) -> Book:
     if not book:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Book with ID {book_id} not found",
+            detail=f"Book not found",
         )
     return JSONResponse(
         status_code=status.HTTP_200_OK, content=book.model_dump()
